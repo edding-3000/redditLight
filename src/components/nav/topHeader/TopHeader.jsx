19d63330af05bbdd0ae65bffb13a4642924ffe16
edding-3000 <sebastian-hauser@outlook.de>
2024-06-29 00:19:14 +0200

    styling desktop navigation

--- src/components/nav/topHeader/TopHeader.jsx
+++ src/components/nav/topHeader/TopHeader.jsx
@@ -27,12 +27,10 @@ export default function TopNav({ toggleMenue, menueOpen }) {
     }
 
     // Set height of navigation as css var
-    const [height, setHeight] = useState(0)
     const ref = useRef(null)
 
     useEffect(() => {
-        setHeight(ref.current.clientHeight);
-        if (height > 0) document.documentElement.style.setProperty("--headerHeight", height + "px");
+        if (ref.current.clientHeight > 0) document.documentElement.style.setProperty("--headerHeight", ref.current.clientHeight + "px");
     });
 
     function mainButtonClick() {
@@ -48,7 +46,9 @@ export default function TopNav({ toggleMenue, menueOpen }) {
                 <span className={`headerRight ${menueOpen ? "open" : ""}`}>
                     <h1><button onClick={mainButtonClick}>redditLight</button></h1>
                     <form onSubmit={handleSubmit}>
-                        <input type="submit" />
+                        <button type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M782-82 523-341q-29 20-67.5 32T372-297q-118 0-200.5-82.5T89-580q0-118 82.5-200.5T372-863q118 0 200.5 82.5T655-580q0 46-12 83.5T611-431l260 261-89 88ZM372-423q66 0 111.5-45.5T529-580q0-66-45.5-111.5T372-737q-66 0-111.5 45.5T215-580q0 66 45.5 111.5T372-423Z" /></svg>
+                        </button>
                         <input placeholder="Search" onChange={searchChange} value={searchQuery} type="text" />
                     </form>
                 </span>
